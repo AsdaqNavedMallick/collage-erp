5cc0e4ff3a51473261658a536b56a06413dca33e
AsdaqNavedMallick <asdaqnavedmallick@gmail.com>
2026-05-10 04:33:49 +0530

    Implemented JWT authentication system

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 const bcrypt = require("bcrypt");
@@ -32,6 +33,60 @@ router.post("/register", async (req, res) => {
   } catch (error) {
     res.status(500).json(error);
   }
+});
+
+// LOGIN USER
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  // FIND USER
+  const sql = "SELECT * FROM users WHERE email = ?";
+
+  db.query(sql, [email], async (err, result) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    // USER NOT FOUND
+    if (result.length === 0) {
+      return res.status(401).json({
+        message: "User not found",
+      });
+    }
+
+    const user = result[0];
+
+    // CHECK PASSWORD
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    // WRONG PASSWORD
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid password",
+      });
+    }
+
+    // CREATE TOKEN
+    const token = jwt.sign(
+      {
+        id: user.user_id,
+        role: user.role,
+      },
+
+      process.env.JWT_SECRET,
+
+      {
+        expiresIn: "1d",
+      },
+    );
+
+    // SUCCESS
+    res.json({
+      message: "Login successful",
+
+      token,
+    });
+  });
 });
 
 module.exports = router;
